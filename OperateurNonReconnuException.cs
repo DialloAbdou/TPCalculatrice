f793c19a110674654b2cb4860a9b1572cbd28f17
DIALLOAbdou <=dialloabdou@hotmail.com>
2023-10-18 22:13:00 +0200

    gestion des Erreur

---
+++ OperateurNonReconnuException.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace TPCalculatrice;
+
+public class OperateurNonReconnuException : Exception
+{
+    public string _operateur { get; set; } = "";
+
+    public OperateurNonReconnuException(string operateur):base($"l'operateur {operateur} n'est pas reconnu")
+    {
+        if (string.IsNullOrWhiteSpace(operateur))
+        {
+            throw  new ArgumentNullException($"'{nameof(_operateur)}' ne peut avoir une valeur null ou de l'espace vide {nameof(_operateur)}");
+
+        }
+        _operateur = operateur;
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Reflection;
 using Namespace;
+using TPCalculatrice;
 int getValeur(string valeur)
 {
     int? resultat = null;
@@ -32,81 +33,40 @@ operandeGauche = getValeur("premiere");
 operandeDroite = getValeur("deuxième");
 
 //====== Operation ======
-Operation? operation = null;
-Operation? operationSeconde = null;
 // instanciation du Constructeur
-Console.WriteLine("Veuillez saisir l'operateur pour le calcul");
-string? operateur = Console.ReadLine();
+
 // Utilisation de Switch Expression
-operation = operateur switch
+
+try
 {
-    "+" => new Addition(operandeGauche, operandeDroite),
-    "-" =>  new Soustraction(operandeGauche, operandeDroite),
-    "*" => new Multiplication(operandeGauche, operandeDroite),
-    "/" => new Division(operandeGauche, operandeDroite),
-    "%" => new Modulo(operandeGauche, operandeDroite),
-    _ => null
-};
-//switch (operateur)
-//{
-//    case "+":
-//        operation = new Addition(operandeGauche, operandeDroite);
-////        operationSeconde = new Addition(operandeGauche, operandeDroite);
-////        Console.WriteLine($"les deux operation sont égaux ? {operation.Equals(operationSeconde)}");
-//        break;
-//    case "-":
-//        operation = new Soustraction(operandeGauche, operandeDroite);
-//        break;
-//    case "*":
-//        operation = new Multiplication(operandeGauche, operandeDroite);
-//        break;
-//    case "/":
-//        operation = new Division(operandeGauche, operandeDroite);
-//        break;
-//    case "%":
-//        operation = new Modulo(operandeGauche, operandeDroite);
-//        break;
-//    default:
-//        operation = null;
-//        break;
+    Console.WriteLine("Veuillez saisir l'operateur pour le calcul");
+    string? operateur = Console.ReadLine();
+    if (operateur is not null)
+    {
+        Operation operation = operateur switch
+        {
+            "+" => new Addition(operandeGauche, operandeDroite),
+            "-" => new Soustraction(operandeGauche, operandeDroite),
+            "*" => new Multiplication(operandeGauche, operandeDroite),
+            "/" => new Division(operandeGauche, operandeDroite),
+            "%" => new Modulo(operandeGauche, operandeDroite),
+            _ => throw new OperateurNonReconnuException(operateur)
+        };
+        Calculatrice calculatrice = new Calculatrice(operation);
+        calculatrice.Executer();
+        Console.WriteLine($"le resultat {calculatrice._operation.ToString()} est :{calculatrice._operation.Resultat}");
 
-//}
+    }else 
+    {
+        Console.WriteLine("aucun operateur a été saisie !");
+    }
 
-
-
-
-
-//if (operateur == "+")
-//{
-//    operation = new Addition(operandeGauche, operandeDroite);
-//    operationSeconde = new Addition(operandeGauche, operandeDroite);
-//    Console.WriteLine($"les deux operation sont égaux ? {operation.Equals(operationSeconde)}");
-//}
-//else if (operateur == "-")
-//{
-//    operation = new Soustraction(operandeGauche, operandeDroite);
-//}
-//else if (operateur == "*")
-//{
-//    operation = new Multiplication(operandeGauche, operandeDroite);
-
-//}
-//else if (operateur == "/")
-//{
-//    operation = new Division(operandeGauche, operandeDroite);
-//}
-//else
-//{
-//    operation = new Modulo(operandeGauche, operandeDroite);
-
-//}
-Calculatrice calculatrice = new Calculatrice(operation);
-calculatrice.Executer();
-Console.WriteLine($"le resultat {calculatrice._operation.ToString()} est :{calculatrice._operation.Resultat}");
-
-
-
-
-
-
-
+}
+catch (OperateurNonReconnuException e)
+{
+    Console.WriteLine($"le resultat renvoie une opération null {e.Message}");
+   
+}catch(Exception e)
+{
+    Console.WriteLine($"Erreur genera ! {e.Message}");
+}
